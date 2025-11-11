7571a2531da9482854b6bf56abee88db3f510164
Surfs-Up5 <cgparsons5@gmail.com>
2025-11-11 17:06:10 -0500

    set up more parent/child classes

--- Employee.java
+++ Employee.java
@@ -1,3 +1,14 @@
-public class Employee {
-    
+public class Employee extends Person {
+    private String employeeID;
+    private String department;
+
+    public Employee(String firstName, String lastName, String employeeID, String department) {
+        super(firstName, lastName);
+        this.employeeID = employeeID;
+        this.department = department;
+    }
+
+    public String getFileData() {
+        return super.getFileData() + "," + employeeID + "," + department;
+    }
 }
--- Tier2Employee.java
+++ Tier2Employee.java
@@ -1,3 +1,10 @@
-public class Tier2Employee {
+public class Tier2Employee extends Employee {
+    private String certification;
+
+    public Tier2Employee(String firstName,  String lastName, String EmployeeID, String department, String certification) {
+        super(firstName, lastName, EmployeeID, department);
+        this.certification = certification;
+    }
+    
     
 }
